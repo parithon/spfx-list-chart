780e8a68f0f35c922f8f3ca974fb40965645163a
Anthony Conrad <parithon@outlook.com>
2018-04-11 17:13:45 -0700

    Added the ability to retrieve lists from SharePoint (#7)

--- src/services/ISPService.ts
+++ src/services/ISPService.ts
@@ -1,8 +1,18 @@
-import { ISPWeb } from "../common/SPEntities";
+import { ISPWeb, ISPList } from "../common/SPEntities";
 import ServiceScope from "@microsoft/sp-core-library/lib/serviceScope/ServiceScope";
 import IWebPartContext from "@microsoft/sp-webpart-base/lib/core/IWebPartContext";
 
+export enum ListQueryOrderBy {
+  Id = 1,
+  Title
+}
+export interface ISPListQueryOptions {
+  baseTemplate?: number;
+  includeHidden?: boolean;
+  orderBy?: ListQueryOrderBy;
+}
 export interface ISPService {
   getRootWeb(): Promise<string>;
   getSites(rootWebUrl: string): Promise<ISPWeb[]>;
+  getLists(siteUrl: string, queryOptions?: ISPListQueryOptions): Promise<ISPList[]>;
 }
